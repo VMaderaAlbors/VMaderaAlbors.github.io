3e234c326e4c6c8ca79d6d2e232fef4b3629a06c
Victor Madera <v.maderaalbors@gmail.com>
2023-11-23 23:58:48 +0000

    finishing up
    finalising links, and adding some javascript behavior

---
+++ GitHubFiles/Project folder/js/homepage.js
@@ -0,0 +1,19 @@
+let swich = true;
+let videoPlace = document.querySelector('video');
+function changeVideo() {
+
+
+    let hex = 'images/pexels-rostislav-uzunov-9629254 (1080p).mp4';
+    // let hex = `<source src="images/pexels-rostislav-uzunov-9629254 (1080p).mp4" type="video/mp4">`;
+    let wave = 'images/pexels-rostislav-uzunov-8303104 (1080p).mp4';
+    if (swich == true) {
+        console.log('working');
+        videoPlace.src = wave;
+        swich = false;
+    } else {
+        console.log('working');
+        videoPlace.src = hex;
+        swich = true;
+    }
+
+}
