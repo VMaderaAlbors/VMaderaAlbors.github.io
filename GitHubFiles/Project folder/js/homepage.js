f1ea725300eede6b28f36d238c44549ac5c60fe4
Victor Madera <v.maderaalbors@gmail.com>
2023-11-24 12:15:15 +0000

    changes done to validate HTML

--- GitHubFiles/Project folder/js/homepage.js
+++ GitHubFiles/Project folder/js/homepage.js
@@ -3,9 +3,9 @@ let videoPlace = document.querySelector('video');
 function changeVideo() {
 
 
-    let hex = 'images/pexels-rostislav-uzunov-9629254 (1080p).mp4';
+    let hex = 'images/pexels-rostislav-uzunov-9629254%20(1080p).mp4';
     // let hex = `<source src="images/pexels-rostislav-uzunov-9629254 (1080p).mp4" type="video/mp4">`;
-    let wave = 'images/pexels-rostislav-uzunov-8303104 (1080p).mp4';
+    let wave = 'images/pexels-rostislav-uzunov-8303104%20(1080p).mp4';
     if (swich == true) {
         console.log('working');
         videoPlace.src = wave;
