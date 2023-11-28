848ce359e88be852647000d6eb2e91794b9a101f
eucarystr <148537605+eucarystr@users.noreply.github.com>
2023-11-28 16:21:52 +0000

    Update script.js

--- GitHubFiles/Project folder/js/script.js
+++ GitHubFiles/Project folder/js/script.js
@@ -1,19 +1,5 @@
-// Contact Form validation
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission by default
-    
-    var userAnswer = parseInt(document.getElementById('answer').value, 10);
-
-    if (userAnswer !== 4) {
-        alert('Incorrect answer. Please try again.');
-    }
-});
-
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    validateForm(event);
-});
-
-function validateForm(event) {
+// Modify the validateForm function to return a boolean
+function validateForm() {
     event.preventDefault(); // Prevent form submission by default
 
     const name = document.getElementById('name').value;
@@ -21,24 +7,34 @@ function validateForm(event) {
 
     if (name.trim() === '') {
         displayError('name-error', 'Name is required');
+        return false;
     } else {
         hideError('name-error');
     }
 
     if (email.trim() === '') {
         displayError('email-error', 'Email is required');
+        return false;
     } else if (!isValidEmail(email)) {
         displayError('email-error', 'Enter a valid email address');
+        return false;
     } else {
         hideError('email-error');
     }
 
     if (name.trim() !== '' && isValidEmail(email)) {
         displaySuccess('success-message', 'Form submitted successfully!');
+        return true;
     } else {
         hideSuccess('success-message');
+        return false;
     }
 }
+
+// Inside the 'validateForm' function, pass the 'event' parameter
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    validateForm(event);
+});
 
 
 
